71ee82b4d8a1a3ba245be18f7dbefd8d019d482a
Andres221-uy <andres221.uy@gmail.com>
2026-05-24 01:30:58 -0300

    Estructura inicial

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const APP_NAME = "CarritosUY";
+
+export const MAP_CONFIG = {
+  lat: -34.9011,
+  lng: -56.1645,
+  zoom: 13
+};
+
+export const SUPABASE_URL = "TU_URL";
+export const SUPABASE_KEY = "TU_KEY";
---
+++ js/state.js
@@ -0,0 +1,11 @@
+export const state = {
+  map: null,
+  markers: [],
+  carritoSeleccionado: null,
+  usuario: null,
+  favoritos: [],
+  filtros: {
+    categoria: "Todos",
+    busqueda: ""
+  }
+};
